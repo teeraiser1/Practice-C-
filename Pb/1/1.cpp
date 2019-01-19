d0840eef241825d32c579e5e2664a66cca79e959
teeraiser1 <teeraiser1>
2019-01-19 13:15:02 +0000

    Make first file

---
+++ Pb/1/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(void)
+{
+	int num = 20;
+	int temp_int;
+	double temp_double;
+	std::cout<<"GE"<<std::endl;
+	std::cout<<"num = "<<num;
+	std::cout<<"GE"<<std::endl;
+	
+	std::cin>>temp_double>>temp_int;
+	std::cout<<temp_int<<"  "<<temp_double<<std::endl;
+}
