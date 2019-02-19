cd1dc3a1f172fe73547749bb79a8fdb298f86589
teeraiser1 <teeraiser1>
2019-02-19 13:42:08 +0000

    19.02.19

--- Pb/4/3-2.cpp
+++ Pb/4/3-2.cpp
@@ -10,19 +10,19 @@ namespace COMP_POS
 
 class NameCard
 {
-	char* name, *coopName, *phoneNum;
+	char* name, *compName, *phoneNum;
 	int pos;
 	public:
-		NameCard(const char* myName, const char* myCoopName, const char* myPhoneNum, int myPos)
+		NameCard(const char* myName, const char* myCompName, const char* myPhoneNum, int myPos)
 			: pos(myPos)
 		{
 			int templen = strlen(myName)+1;
 			name = new char[templen];
 			strcpy(name, myName);
 			
-			templen = strlen(myCoopName)+1;
-			coopName = new char[templen];
-			strcpy(coopName, myCoopName);
+			templen = strlen(myCompName)+1;
+			compName = new char[templen];
+			strcpy(compName, myCompName);
 				
 			templen = strlen(myPhoneNum)+1;
 			phoneNum = new char[templen];
@@ -31,9 +31,15 @@ class NameCard
 		void ShowNameCardInfo()
 		{
 			cout<<"이름 : "<<name<<endl;
-			cout<<"회사 : "<<coopName<<endl;
+			cout<<"회사 : "<<compName<<endl;
 			cout<<"전화번호 : "<<phoneNum<<endl;
 			cout<<"직급 : "<<COMP_POS::STRING[pos]<<endl;
+		}
+		~NameCard()
+		{
+			delete []name;
+			delete []compName;
+			delete []phoneNum;
 		}
 };
 
@@ -43,7 +49,10 @@ int main(void)
 	NameCard manSenior("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
 	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
 	manClerk.ShowNameCardInfo();
+	cout<<endl;
 	manSenior.ShowNameCardInfo();
+	cout<<endl;
 	manAssist.ShowNameCardInfo();
+	cout<<endl;
 	return 0;
 }
