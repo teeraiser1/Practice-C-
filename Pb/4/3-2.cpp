223d93aed78663e9fe96c2df960102fc340ab706
teeraiser1 <teeraiser1>
2019-02-19 13:39:20 +0000

    19.02.19

--- Pb/4/3-2.cpp
+++ Pb/4/3-2.cpp
@@ -2,12 +2,18 @@
 #include<cstring>
 using namespace std;
 
+namespace COMP_POS
+{
+	enum {CLERK, SENIOR, ASSIST, MANGER};
+	char STRING[4][10] = {"사원", "주임", "대리", "매니저"};
+}
+
 class NameCard
 {
 	char* name, *coopName, *phoneNum;
 	int pos;
 	public:
-		NameCard(char* myName, char* myCoopName, char* myPhoneNum, int myPos)
+		NameCard(const char* myName, const char* myCoopName, const char* myPhoneNum, int myPos)
 			: pos(myPos)
 		{
 			int templen = strlen(myName)+1;
@@ -22,8 +28,22 @@ class NameCard
 			phoneNum = new char[templen];
 			strcpy(phoneNum, myPhoneNum);
 		}
-	
+		void ShowNameCardInfo()
+		{
+			cout<<"이름 : "<<name<<endl;
+			cout<<"회사 : "<<coopName<<endl;
+			cout<<"전화번호 : "<<phoneNum<<endl;
+			cout<<"직급 : "<<COMP_POS::STRING[pos]<<endl;
+		}
 };
 
-enum {CLERK, SENIOR, ASSIST, MANGER} COMP_POS;
-
+int main(void)
+{
+	NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLERK);
+	NameCard manSenior("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
+	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
+	manClerk.ShowNameCardInfo();
+	manSenior.ShowNameCardInfo();
+	manAssist.ShowNameCardInfo();
+	return 0;
+}
